c78af138d199f40db28abe76893056454ba4c6e1
Budiarto Herman <buherman1010@gmail.com>
2021-03-18 06:17:56 +0700

    Added test scripts

---
+++ test_high.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python test1.py 100
+python test1.py 101 # ERROR
---
+++ test_low.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+python test1.py 0 # ERROR
+python test1.py 1
+python test1.py 2
+python test1.py 3
+python test1.py 4
+python test1.py 5
+python test1.py 6
+python test1.py 7
